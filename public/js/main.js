023b3c0e48e001d8c3a6152536a0c3f4296f22bd
listingslab <listingslab@gmail.com>
2015-09-14 19:54:25 +0100

    npm init

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    ipt = $('#inputNumber');
+    ipt.focus ();
+    ipt.keyup(function() {
+	  validateInput();
+	});
 
+});
+
+function validateInput() { 
+	console.log (ipt.val());
+}
